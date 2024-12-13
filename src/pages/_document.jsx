174d038cad235c69fa740f1bcb8f4ed4827ce059
ducnguyen1002 <133721410+ducnguyen1002@users.noreply.github.com>
2024-12-13 15:25:00 +0700

    add robots.txt & sitemap

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -9,11 +9,11 @@ export default function Document() {
         <link
           rel="preload"
           href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
-          as="font"
+          as="style"
         />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
-          rel="font"
+          rel="stylesheet"
         />
       </Head>
       <body className="antialiased">
