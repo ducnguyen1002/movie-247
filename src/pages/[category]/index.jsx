415ed70d1b48493c46a015e3033f789c34ba6357
ducnguyen1002 <133721410+ducnguyen1002@users.noreply.github.com>
2025-01-14 09:31:25 +0700

    .

--- src/pages/[category]/index.jsx
+++ src/pages/[category]/index.jsx
@@ -45,7 +45,7 @@ const CategoryPage = ({ movies, categorySlug }) => {
         <meta property="og:image" content={seoOnPage.og_images} />
       </Head>
       <div className="container mx-auto px-4 py-16 lg:px-10 lg:py-20 bg-gray-950">
-        <h1 className="mb-2 font-semibold text-3xl text-white text-center">Phim {category.name}</h1>
+        <h1 className="mb-2 font-semibold text-3xl text-white text-center">{category.name}</h1>
         <hr
           className="mx-auto mb-6 w-1/4 h-[1px] border-0 bg-gradient-to-r from-gray-700 via-gray-200 to-gray-700" />
           
